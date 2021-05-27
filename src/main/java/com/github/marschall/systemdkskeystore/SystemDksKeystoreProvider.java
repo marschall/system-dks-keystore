10bf1506d6651654a44b8ab6a8ed78e698d2e1f5
Philippe Marschall <philippe.marschall@gmail.com>
2021-05-27 18:48:05 +0200

    bump

---
+++ src/main/java/com/github/marschall/systemdkskeystore/SystemDksKeystoreProvider.java
@@ -0,0 +1,21 @@
+package com.github.marschall.systemdkskeystore;
+
+import java.security.Provider;
+
+public final class SystemDksKeystoreProvider extends Provider {
+
+  /**
+   * The name of this security provider.
+   */
+  public static final String NAME = "system-DKS";
+
+  /**
+   * The type of keystore that uses directories to store certificates.
+   */
+  public static final String TYPE = "system-DKS";
+
+  public SystemDksKeystoreProvider() {
+    super(NAME, 1.0d, "system-DKS (KeyStore)");
+  }
+
+}
